913f16de0604c68aa851f65ed88f18afc613d8c8
Keerthana Natarajan <67564209+Keerthana0309@users.noreply.github.com>
2020-08-12 05:04:50 -0400

    Update and rename ReverseInteger to ReverseInteger.py

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -1,17 +1,17 @@
-Given a 32-bit signed integer, reverse digits of an integer.
+#Given a 32-bit signed integer, reverse digits of an integer.
 
-Example 1:
+#Example 1:
 
-Input: 123
-Output: 321
-Example 2:
+#Input: 123
+#Output: 321
+#Example 2:
 
-Input: -123
-Output: -321
-Example 3:
+#Input: -123
+#Output: -321
+#Example 3:
 
-Input: 120
-Output: 21
+#Input: 120
+#Output: 21
 
 
 class Solution:
